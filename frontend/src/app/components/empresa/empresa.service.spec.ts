bac835dcf580250a5d99b3e05cfe8171dc745f99
Eduardo <eduardoamorim927@gmail.com>
2022-07-24 10:46:45 -0300

    Testes com empresa service

--- frontend/src/app/components/empresa/empresa.service.spec.ts
+++ frontend/src/app/components/empresa/empresa.service.spec.ts
@@ -1,20 +1,74 @@
+import { OverlayModule } from '@angular/cdk/overlay';
 import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
-import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 
 import { EmpresaService } from './empresa.service';
 
 describe('EmpresaService', () => {
   let service: EmpresaService;
+  let injector: TestBed;
+  let httpClient: HttpClient;
+  let httpMock: HttpTestingController;
+  let mockResponse = {
+    nome: 'Empresa teste',
+    email: 'teste@teste.com',
+    cnpj: '19349699000128',
+    endereco: 'Rua teste',
+    senha: '123456'
+  };
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
-      imports: [ MatSnackBarModule, HttpClientModule ]
+      imports: [ MatSnackBarModule, HttpClientModule, RouterTestingModule, OverlayModule,
+        HttpClientTestingModule, BrowserAnimationsModule ]
     })
     service = TestBed.inject(EmpresaService);
+    injector = getTestBed();
+    httpClient = TestBed.inject(HttpClient);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it(`#${EmpresaService.prototype.read.name} should read a company`, () => {
+    let showCompany;
+    let baseUrl = 'http://localhost:3001/empresas/';
+
+    spyOn(httpClient, 'get').and.returnValue(of(mockResponse));
+
+    service.read().subscribe((res) => {
+      showCompany = res;
+    })
+
+    expect(showCompany).toEqual(mockResponse);
+    expect(httpClient.get).toHaveBeenCalledWith(baseUrl);
+    httpMock.verify();
+  });
+
+  it(`#${EmpresaService.prototype.create.name} should create a company`, () => {
+    let empresa =  {
+      nome: 'Empresa teste',
+      email: 'teste@teste.com',
+      cnpj: '19349699000128',
+      endereco: 'Rua teste',
+      senha: '123456'
+    }
+
+    spyOn(httpClient, 'post').and.returnValue(of(mockResponse));
+
+    service.create(empresa).subscribe(res => {
+      res = empresa;
+    })
+    
+    expect(httpClient.post).toHaveBeenCalledTimes(1);
+    expect(empresa).toEqual(mockResponse);
+    httpMock.verify();
   });
 });
